5b0009e930e78e1f896397431db71d75c7ebdaba
Shae Smith <sbsmith86@gmail.com>
2016-08-22 17:17:16 -0400

    App setup

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Todo;
+
+use App\Http\Requests;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todos = Todo::orderBy('created_at', 'asc')->get();
+
+        return view('todos', [
+            'todos' => $todos
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = \Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $task = new Todo;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/todo');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+
+        return redirect('/');
+    }
+}
---
+++ resources/views/todos.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <!-- Bootstrap Boilerplate... -->
+
+    <div class="panel-body">
+        <!-- Display Validation Errors -->
+        @include('common.errors')
+
+        <!-- New todo Form -->
+        <form action="{{ url('todo') }}" method="POST" class="form-horizontal">
+            {{ csrf_field() }}
+
+            <!-- todo Name -->
+            <div class="form-group">
+                <label for="todo" class="col-sm-3 control-label">Todo</label>
+
+                <div class="col-sm-6">
+                    <input type="text" name="name" id="todo-name" class="form-control">
+                </div>
+            </div>
+
+            <!-- Add Todo Button -->
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-plus"></i> Add Todo
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <!-- Current todos -->
+    <!-- Current todos -->
+    @if (count($todos) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Current todos
+            </div>
+
+            <div class="panel-body">
+                <table class="table table-striped todo-table">
+
+                    <!-- Table Headings -->
+                    <thead>
+                        <th>todo</th>
+                        <th>&nbsp;</th>
+                    </thead>
+
+                    <!-- Table Body -->
+                    <tbody>
+                        @foreach ($todos as $todo)
+                            <tr>
+                                <!-- todo Name -->
+                                <td class="table-text">
+                                    <div>{{ $todo->name }}</div>
+                                </td>
+
+                                <td>
+                                    <!-- Delete Button -->
+                                    <td>
+                                        <form action="{{ url('todo/'.$todo->id) }}" method="POST">
+                                            {{ csrf_field() }}
+                                            {{ method_field('DELETE') }}
+
+                                            <button type="submit" class="btn btn-danger">
+                                                <i class="fa fa-trash"></i> Delete
+                                            </button>
+                                        </form>
+                                    </td>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
+@endsection
