517e160c3601f127dbf6a842904fafb1ab775cfa
Shae Smith <sbsmith86@gmail.com>
2016-08-22 17:29:54 -0400

    Clear up unused methods

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -24,16 +24,6 @@ class TodoController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -66,30 +56,7 @@ class TodoController extends Controller
      */
     public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
+        // Write the code for your assignment here.
     }
 
     /**
