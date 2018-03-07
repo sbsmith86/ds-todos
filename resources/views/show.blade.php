91cd82b1325827241d0795a392fa3ac1b2fad3ef
Kevin Deguzman <deguzmankevin@hotmail.com>
2018-03-07 14:07:53 -0800

    styled show page

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,19 +2,19 @@
 <link rel="stylesheet"href="{{ URL::asset('css/show.css') }}">
 @section('content')
 
-
+<div class="content">
 <h1>DETAILS</h1>
 <h2>Name: {{$name}}</h2>
 <h2>Description: {{$description}}</h2>
 
  <!-- Home Button -->
-<td>
+
   <form action="{{ url('/') }}" method="GET">
     {{ csrf_field() }}
-      <button type="submit" class="btn btn-default">
+      <button type="submit" class="btn btn-secondary">
           <i class="fa fa-trash"></i> Home
       </button>
     </form>
-</td>
 
+</div>
 @endsection
